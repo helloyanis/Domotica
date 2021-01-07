975eebc00a2d94c4d1a87e115ebf0be10ab03301
helloyanis <helloyanis@hotmail.com>
2021-01-07 15:50:40 +0000

    aa

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const menuButton= document.querySelector(".btn");
 menuButton.addEventListener("click",function(){
   backdropElement.classList.toggle("active");
 });
+
+
+/*Voir les valeurs de l'input range température*/
+$(function() {
+	$('.temp').next().text('25'); // Valeur par défaut
+	$('.temp').on('input', function() {
+		var $set = $(this).val();
+		$(this).next().text($set);
+	});
+});
